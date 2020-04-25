9a1ae8ceaaad7efd10399174949926c8d6d71454
Hem <hem@Hems-MacBook-Pro.local>
2020-04-25 17:45:13 +0530

    first commit

---
+++ spring-config-server/src/main/java/com/spring/config/springconfigserver/SpringConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.config.springconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigServerApplication.class, args);
+	}
+
+}
